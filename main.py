d87fa1f35d6ddaa4271127efc41338d76f8342fb
kozlovskaya-oa <94537045+kozlovskaya-oa@users.noreply.github.com>
2022-05-04 23:47:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -54,14 +54,11 @@ def carousel():
 <div class="container">
   <h2>Пейзажи Марса</h2>  
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
       <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
-
-    <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
         <img src="{url_for('static', filename='img/slide_1.jpg')}" alt="image 1" style="width:100%;">
@@ -75,7 +72,6 @@ def carousel():
         <img src="{url_for('static', filename='img/slide_3.jpg')}" alt="image 3" style="width:100%;">
       </div>
     </div>
-    <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
       <span class="sr-only">Previous</span>
